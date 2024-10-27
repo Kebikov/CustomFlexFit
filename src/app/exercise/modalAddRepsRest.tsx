dce7ba0af270bc4665aab08c3a7aba859bd4f9d3
Kebikov Evgenii <kebikovgen@gmail.com>
2024-10-28 00:40:59 +0300

    made state for save time in modalAddRepsRest

--- src/app/exercise/modalAddRepsRest.tsx
+++ src/app/exercise/modalAddRepsRest.tsx
@@ -11,18 +11,24 @@ import Clock, { ITimeClock, IClockRef } from '@/components/Clock/Clock';
  */
 const modalAddRepsRest: FC = () => {
 
+
     /**
- * @param electedTime Выбранное время.
- */
-const [selectedTime, setSelectedTime] = useState<ITimeClock>({one: 30, two: 0});
-    console.log('selectedTime >>> ', selectedTime);
+     * @param runtime Длительность выполнения упражнения.
+     */
+    const [runtime, setRuntime] = useState<ITimeClock>({one: 30, two: 0});
+    console.log('runtime >>> ', runtime);
+    /**
+     * @param restAfter Время отдыха после упражнения.
+     */
+    const [restAfter, setRestAfter] = useState<ITimeClock>({one: 30, two: 0});
+    console.log('restAfter >>> ', restAfter);
 
-const refClock = useRef<IClockRef>(null);
+    const refRestAfter = useRef<IClockRef>(null);
+    const refRuntime = useRef<IClockRef>(null);
 
-const press = () => {
-    console.log('press');
-    refClock.current?.openClock();
-}
+    const onRestAfter = () => refRestAfter.current?.openClock();
+    const onRuntime = () => refRuntime.current?.openClock();
+    
 
     return (
         <WrapperScroll
@@ -30,15 +36,34 @@ const press = () => {
         >
             <HeaderGoBack/>
             <View style={{flex: 1, justifyContent: 'center'}} >
-                <Button onPress={() => press()} title='open' />
+
+                <View style={{width: '100%', marginTop: 20, backgroundColor: 'green'}} >
+                    <Button onPress={() => onRuntime()} title='runtime' color={'white'} />
+                    <Text style={{color: 'white', fontSize: 18}} >{`${runtime.one} мин. ${runtime.two} сек.`}</Text>
+                </View>
+                <View style={{width: '100%', marginTop: 20, backgroundColor: 'blue'}} >
+                    <Button onPress={() => onRestAfter()} title='restAfter' color={'white'} />
+                    <Text style={{color: 'white', fontSize: 18}} >{`${restAfter.one} мин. ${restAfter.two} сек.`}</Text>
+                </View>
+                
+
                 <Clock 
-                    setSelectedTime={setSelectedTime} 
-                    selectedTime={selectedTime} 
+                    setSelectedTime={setRestAfter} 
+                    selectedTime={restAfter} 
                     isUsePortal={false}
                     colorText={COLOR_ROOT.LIME_70}
                     typeClock='minutes_30/seconds'
-                    ref={refClock} 
+                    ref={refRestAfter} 
                 />
+
+                <Clock 
+                    setSelectedTime={setRuntime} 
+                    selectedTime={runtime} 
+                    isUsePortal={false}
+                    colorText={COLOR_ROOT.LIME_70}
+                    typeClock='minutes_30/seconds'
+                    ref={refRuntime} 
+                />      
             </View>
         </WrapperScroll>
     );
