57476094962cafeeef757d6b9e6280e8412f039d
Kebikov Evgenii <kebikovgen@gmail.com>
2025-02-14 21:09:24 +0300

    made function pickObject

--- src/helpers/pickObject.ts
+++ src/helpers/pickObject.ts
@@ -1,46 +1,22 @@
-const ifValueArray = <T>(item: T, value: ['id', ...(keyof T)[]]) => {
-    const newObj: Partial<T> = {};
-    for(const key of value) {
-        newObj[key] = item[key];
-    }
-    return newObj;
-}
+type TValue<T> = ['id', ...(keyof Omit<T, 'id'>)[]];
 
-const ifValueString = <T>(item: T, value: keyof T) => {
-    const newObj: Partial<T> = {};
-    newObj[value] = item[value];
-    return newObj;
-}
-
-const sortArray = <T>(item: T[], value: keyof T | (keyof T)[]) => {
-    return item.map(obj => {
-        if(Array.isArray(value)) {
-            return ifValueArray(obj, value);
-        } else {
-            return ifValueString(obj, value);
-        }
-    });
-}
-
-const sortObject = <T>(item: T, value: keyof T | (keyof T)[]) => {
-    if(Array.isArray(value)) {
-        return ifValueArray(item, value); 
-    } else {
-        return ifValueString(item, value);
-    }
+const filterObject = <T extends {id: number}>(item: T, value: TValue<T>) => {
+    return Object.fromEntries(
+        Object.entries(item).filter(([key, keyValue]) => value.includes(key as 'id' | Exclude<(keyof T), 'id'>))
+    );
 }
 
 
  /** `Выбор свойств из обьекта или массива обьектов.` */
-export const pickObject = <T extends {id: number}>(item: T | T[], value: ['id', ...(keyof T)[]] ) => {
-
+export const pickObject = <T extends {id: number}>(item: T | T[], value: TValue<T> ) => {
     if(Array.isArray(item)) {
         //* Если передан массив для сортировки.
-        return sortArray(item, value);
+        return item.map(obj => {
+            return filterObject(obj, value);
+        });
     } else {
         //* Если передан обьект для сортировки.
-        return sortObject(item, value);
+        return filterObject(item, value);
     }
-
 }
 
